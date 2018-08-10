0db8116fc2c9940c226dd653fcf10b013e74229e
abhijeetckar <ckar.abhi@gmail.com>
2018-08-10 19:19:00 +0530

    8-queen-solved

--- 8-Queen-Problem/queen.js
+++ 8-Queen-Problem/queen.js
@@ -1,83 +1,109 @@
 "use strict";
-const os = require('os');
 
+var os = require('os');
 
-var n = 8;
-
-solveNQ();
-
-function printSolution(array) {
-  //console.log(arr)
-  for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array.length; j++) {
-          //console.log(arr[i][j]);
-          process.stdout.write('[' + String(array[i][j]) + ']');
-      }
-      process.stdout.write(os.EOL);
+function init(board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j] = 'x';
+    }
   }
 }
 
-function isSafe(board, row, col){
-
-  // Checks the ← direction
-  for(var i=0; i<col; i++){
-    if (board[row][i] === 'Q') {
-      return false;
+function createBoard(rows) {
+  var board = new Array();
+  for (let i = 0; i < rows; i++) {
+    board[i] = new Array(rows);
+  }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j] = 'x';
     }
   }
-
-  // Checks the ↖ direction 
-  for(var i=row, j=col; i>=0 && j>=0; i--, j--){
-    if (board[i][j] === 'Q') {
-      return false;
-    }
-  }
-
-  // Checks the ↙ direction 
-  for(var i=row, j=col; j>=0 && i<n; i++, j--){
-    if (board[i][j] === 'Q'){
-      return false;
-    }
-  }
-
-  return true;
+  return board;
 }
 
+function solvePuzzle(rows) {
+  var board = createBoard(rows);
+  for (let col = 0; col < board.length; col++ ){
+      placeQueen(board, 0, col, 0)
+      console.log("\n\n")
+      init(board);
+  }
+}
 
-function recurseNQ(board, col){
-  if(col===n){
-      console.log();
-      printSolution(board); // <-- print another solution when n==8
-    return;  
+function placeQueen(board, row, col, count) {
+  board[row][col] = 'Q';
+  count++;
+
+  //success case
+  if (count == board.length) {
+    printBoard(board);
+    return true;
   }
 
-  for(var i=0; i<n; i++){
-    if(isSafe(board, i, col)){
-      board[i][col]='Q';
+  if (row+1 >= board.length ){
+    return false;
+  }
 
-      recurseNQ(board, col+1);
-      //if(recurseNQ(board, col+1)===true) //<-- you don't need this
-          // return true;
-
-      board[i][col]=0;
+  let placed = false
+  for (let c = 0; c < board.length; c++) {
+    const check = isValidPosition(board, row + 1, c);
+    if (check == true) {
+      placeQueen(board, row + 1, c, count);
+      placed = true;
     }
+  }
+  if (placed == false) {
+    count--;
+    board[row][col] = 'x';
   }
   return false;
 }
 
 
-function solveNQ(){
-  var board = generateBoard(n);
-  recurseNQ(board, 0);
-}
+function isValidPosition(board, row, col) {
 
-function generateBoard(n){
-  var board=[];
-  for(var i=0; i<n; i++){
-    board[i]=[];
-    for(var j=0; j<n; j++){
-      board[i][j]='_';
+  //check horizontal row for Queen
+  for (let i = 0; i < board.length; i++) {
+    if (board[row][i] == 'Q') {
+      return false;
     }
   }
-  return board;
+
+  //check vertical column for queen
+  for (let i = 0; i < board[col].length; i++) {
+    if (board[i][col] == 'Q') {
+      return false;
+    }
+  }
+
+  //check diagonally right upwards
+  for (let i = row, j = col; i >= 0 && j < board.length; i-- , j++) {
+    if (board[i][j] == 'Q') {
+      return false;
+    }
+  }
+
+  //check diagonally left upwards
+  for (let i = row, j = col; i >= 0 && j >= 0; i-- , j--) {
+    if (board[i][j] == 'Q') {
+      return false;
+    }
+  }
+  return true;
 }
+
+
+
+function printBoard(board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      process.stdout.write('[' + board[i][j].toString() + ']');
+    }
+    process.stdout.write(os.EOL);
+  }
+  console.log("\n")
+}
+
+solvePuzzle(4);
